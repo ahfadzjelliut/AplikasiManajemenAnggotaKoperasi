97851f4d032c412cc01ce66d166c4b2e81a634f4
fadz <ahfadz.jelliut@gmail.com>
2026-07-20 15:56:23 +0800

    build app

---
+++ resources/js/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+    return (
+        <h1>Tes Laravel dan react</h1>
+    );
+}
+
+ReactDOM.createRoot(document.getElementById("app")).render(
+    <App />
+)
---
+++ resources/views/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    @vite([
+        'resources/css/app.css',
+        'resources/js/app.jsx'
+    ])
+</head>
+<div id="app"></app>
+</html>
